3594a431c915343d2c3448cfa030e2aa09da84cc
shkherad <shkherad@bu.edu>
2016-06-09 11:21:22 -0400

    my class demo notes added
    class demo notes (shireen)

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,28 @@
 require 'pry'
+
+class Person
+  def initialize(given_name,surname)
+    @given_name = given_name
+    @surname = surname
+    @favorite_food
+    @catchphrase
+  end
+
+  def favorite_food                      # 'getter' for @favorite_food
+    @favorite_food
+  end
+  def favorite_food=(food)              # 'setter' for @favorite_food
+    @favorite_food = food
+  end
+
+  def catchphrase
+    @catchphrase
+  end
+  def catchphrase=(cp)
+    @catchphrase = cp
+  end
+
+
+end
+
+#shireen = Person.new("Shireen","Kheradpey","spaghetti sauce","It's not the size of the dog in a fight, rather it's the size of fight in the dog.")
